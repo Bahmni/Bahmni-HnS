72cbd37d0d03e5f0fe1c73373ed6d065b5ad34d9
Preethi <preethi.ms34@gmail.com>
2017-01-09 10:37:49 +0530

    Preethi, Gaurav | Tried initialising converters

--- health-standard-converter/src/main/java/org/openmrs/healthStandard/converter/Converter.java
+++ health-standard-converter/src/main/java/org/openmrs/healthStandard/converter/Converter.java
@@ -2,6 +2,13 @@ package org.openmrs.healthStandard.converter;
 
 import org.openmrs.OpenmrsObject;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Converter {
     Class<? extends OpenmrsObject> type();
 
