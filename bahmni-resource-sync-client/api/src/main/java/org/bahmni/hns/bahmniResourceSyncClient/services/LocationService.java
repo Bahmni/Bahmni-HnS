4e098c8b10a7f4ab0512dbfab93c618d31845dcc
gauravd <gauravd@thoughtworks.com>
2017-01-17 15:27:11 +0530

    Gaurav,Lavanya | Fix Save Location

--- bahmni-resource-sync-client/api/src/main/java/org/bahmni/hns/bahmniResourceSyncClient/services/LocationService.java
+++ bahmni-resource-sync-client/api/src/main/java/org/bahmni/hns/bahmniResourceSyncClient/services/LocationService.java
@@ -1,6 +1,7 @@
 package org.bahmni.hns.bahmniResourceSyncClient.services;
 
 import org.hl7.fhir.dstu3.model.Location;
+import org.openmrs.api.context.Context;
 import org.openmrs.healthStandard.converter.fhir.FHIRConverterFactory;
 import org.openmrs.healthStandard.converter.fhir.LocationConverter;
 
@@ -8,7 +9,6 @@ public class LocationService {
   public org.openmrs.Location save(Location fhirLocation) {
     LocationConverter locationConverter = (LocationConverter) FHIRConverterFactory.getInstance().getConverterFor(org.openmrs.Location.class);
     org.openmrs.Location openmrsLocation = locationConverter.toEMRResource(fhirLocation);
-//    return Context.getLocationService().saveLocation(openmrsLocation);
-  return  openmrsLocation;
+    return Context.getLocationService().saveLocation(openmrsLocation);
   }
 }
