d62a7c685bd3a490a729ecb3e24f518d2cc49fff
bitweft <bitweft@gmail.com>
2017-01-25 14:42:47 +0530

    Gaurav, Lavanya | #3174 | Add task to retry failed location sync event

---
+++ bahmni-resource-sync-client/omod/src/main/java/org/bahmni/hns/task/LocationResourceFailedEventsSyncTask.java
@@ -0,0 +1,23 @@
+package org.bahmni.hns.task;
+
+import org.bahmni.hns.bahmniResourceSyncClient.BahmniResourceSyncProperties;
+import org.bahmni.hns.bahmniResourceSyncClient.client.AtomFeedClientFactory;
+import org.ict4h.atomfeed.client.service.FeedClient;
+import org.openmrs.scheduler.tasks.AbstractTask;
+
+import java.net.URISyntaxException;
+
+public class LocationResourceFailedEventsSyncTask extends AbstractTask {
+
+
+  @Override
+  public void execute() {
+    BahmniResourceSyncProperties properties = BahmniResourceSyncProperties.getInstance();
+    FeedClient feedClient;
+    try {
+      feedClient = new AtomFeedClientFactory().getFeedClient(properties.getLocationFeedUri(), properties);
+      feedClient.processFailedEvents();
+    } catch (URISyntaxException e) {
+    }
+  }
+}
